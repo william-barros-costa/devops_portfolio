cf24b0851740a3d8f84e54627904287b47baef4f
william-barros-costa <william.de.barros.da.costa@gmail.com>
2025-01-16 17:06:23 +0000

    Added Dockerfile

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module william-barros-costa/devops_portfolio
+
+go 1.23.0
---
+++ src/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		prefix := "/app/"
+		if strings.HasSuffix("/app", r.URL.Path) {
+			prefix = "/app"
+		}
+		http.StripPrefix(prefix, next).ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := &http.ServeMux{}
+	apiCfg := apiConfig{}
+	handler := http.FileServer(http.Dir("."))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "OK")
+	})
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		bytes, err := os.ReadFile("metrics.html")
+		if err != nil {
+			http.Error(w, "Server Error", 500)
+		}
+		fmt.Fprintf(w, string(bytes), apiCfg.fileserverHits.Load())
+	})
+	mux.Handle("/app/", apiCfg.middleWareMetricsInc(handler))
+	mux.Handle("/app", apiCfg.middleWareMetricsInc(handler))
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+	})
+
+	server := http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
